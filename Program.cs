fad52ec051233497ba877573e96b2a2dbf2bad06
yuwei.xie <ivy@yuweixiedeMacBook-Pro.local>
2025-04-27 02:05:22 +1000

    test

--- Program.cs
+++ Program.cs
@@ -1,10 +1,45 @@
 // Program.cs
+<div data-v-e379c7a2="" class="card-body"><div data-v-54877af9="" class="question-html-content narrow copy-protection d-print-none" data-v-e379c7a2=""><p>You<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">y</b> are<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">p</b> holding<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">w</b> one<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">f</b> link<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">k</b> of<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">6</b> a<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">z</b> chain<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">i</b> in<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">y</b> your<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">b</b> hand.<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">6</b> Implement<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">i</b> method<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">8</b> <em>LongerSide</em><b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">k</b> to<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">s</b> find<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">z</b> which<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">6</b> side<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">p</b> of<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">8</b> the<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">b</b> chain,<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">4</b> relative<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">5</b> to<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">z</b> the<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">x</b> link<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">z</b> you<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">z</b> are<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">t</b> holding,<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">1</b> has<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">7</b> more<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">1</b> links.</p>
+<p>If<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">n</b> the<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">k</b> left<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">f</b> side<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">2</b> has<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">2</b> more<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">f</b> links<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">x</b> return<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">g</b> <em>Side.Left</em>,<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">w</b> if<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">b</b> the<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">u</b> right<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">b</b> side<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">n</b> has<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">c</b> more<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">j</b> links<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">m</b> return<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">n</b> <em>Side.Right</em>,<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">v</b> and<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">j</b> if<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">9</b> both<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">9</b> sides<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">s</b> have<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">d</b> an<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">r</b> equal<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">j</b> number<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">1</b> of<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">4</b> links<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">j</b> or<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">c</b> if<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">u</b> the<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">m</b> chain<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">3</b> is<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">k</b> a<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">v</b> closed<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">a</b> loop,<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">e</b> return<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">a</b> <em>Side.None</em>.<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">x</b> For<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">8</b> example,<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">n</b> for<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">d</b> the<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">a</b> code<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">2</b> below,<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">v</b> the<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">c</b> output<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">b</b> should<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">y</b> be<b style="display: none; width: 0px; color: rgb(255, 255, 255); font-size: 25%;">m</b> <em>Side.Right</em>:</p>
+<pre><code class="language-cs hljs csharp">ChainLink left = <span class="hljs-keyword">new</span> ChainLink();<font></font>
+ChainLink middle = <span class="hljs-keyword">new</span> ChainLink();<font></font>
+ChainLink right = <span class="hljs-keyword">new</span> ChainLink();<font></font>
+left.Append(middle);<font></font>
+middle.Append(right);<font></font>
+Console.WriteLine(left.LongerSide());</code></pre></div></div>
 using System;
 
-Person p = new("Alice");
-p.SayHello();
+public enum Side { None, Left, Right }
 
-public class Person(string name) 
+public class ChainLink
 {
-    public void SayHello() => Console.WriteLine($"Hi, I'm {name}");
+    public ChainLink Left { get; private set; }
+    public ChainLink Right { get; private set; }
+    
+    public void Append(ChainLink rightPart)
+    {
+        if (this.Right != null)
+            throw new InvalidOperationException("Link is already connected.");
+
+        this.Right = rightPart;
+        rightPart.Left = this;
+    }
+
+    
+
+    public Side LongerSide()
+    {
+        const leftCount = this
+        throw new InvalidOperationException("Waiting to be implemented.");
+    }
+
+    public static void Main(string[] args)
+    {
+        ChainLink left = new ChainLink();
+        ChainLink middle = new ChainLink();
+        ChainLink right = new ChainLink();
+        left.Append(middle);
+        middle.Append(right);
+        Console.WriteLine(left.LongerSide());
+    }
 }
